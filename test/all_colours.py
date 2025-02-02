e220968db0396de8d962805f06156a9da86beb9e
Lau Yee-Yu <liuyiyu2003@126.com>
2025-02-02 22:23:22 +0800

    test: add tests for colour picker and colour dictionaries

---
+++ test/all_colours.py
@@ -0,0 +1,23 @@
+"""Make a plot of all the colours in the palette."""
+
+import plotpalette
+
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    # Create a list of all the main colours
+    names = list(plotpalette.palette.keys())
+    x_pos = range(len(names))
+    colours = list(plotpalette.palette.values())
+    height = [1] * len(names)
+
+    # Create a figure and axis
+    fig, ax = plt.subplots(figsize=(15,5))
+    ax.bar(x_pos, height, color=colours, hatch='', edgecolor='white')
+    ax.set_xticks(x_pos)
+    ax.set_xticklabels(names, rotation=45, ha='right')
+    ax.set_yticks([])
+
+    # Show the plot
+    plt.tight_layout()
+    plt.show()
---
+++ test/pick_colour.py
@@ -0,0 +1,26 @@
+"""Pick the colours."""
+
+import plotpalette
+
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    # Create a list of all the main colours
+    picked_colours = plotpalette.pick_colours(2, 2, 'monza')
+    colours = [colour for colour_family in picked_colours for colour in colour_family]
+    names = [str(i) for i in range(len(colours))]
+    print(colours)
+    x_pos = range(len(names))
+    height = [1] * len(names)
+
+    # Create a figure and axis
+    fig, ax = plt.subplots(figsize=(15,5))
+    ax.bar(x_pos, height, color=colours, hatch='', edgecolor='white')
+    ax.set_xticks(x_pos)
+    ax.set_xticklabels(names, rotation=45, ha='right')
+    ax.set_yticks([])
+
+    # Show the plot
+    plt.tight_layout()
+    plt.show()
+
