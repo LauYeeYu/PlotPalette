3901b14325360a46d11e93b0168e0b8fc5ab4254
Lau Yee-Yu <liuyiyu2003@126.com>
2025-02-04 23:03:13 +0800

    refactor(colour picker): allow num_sub_colours to be a list

--- plotpalette/colour_picker.py
+++ plotpalette/colour_picker.py
@@ -2,9 +2,24 @@
 
 from .colours import name, main_palette, palette
 
-__all__ = ['pick_colours']
+__all__ = ['pick_colours_in_one_family', 'pick_colours']
 
-def pick_colours_in_one_family(num_sub_colours: int, main_colour: str) -> list:
+
+def pick_colours_in_one_family(
+        num_sub_colours: int,
+        main_colour: str
+) -> list:
+    """ Pick colours in one family.
+
+    Args:
+        num_sub_colours (int): The number of sub colours to pick.
+        main_colour (str): The main colour to pick. You may use the name
+                           of colour in the main palette (e.g., silverstone)
+                           or the colour (e.g., yellow).
+
+    Returns:
+        list: A list of colours.
+    """
     match num_sub_colours:
         case 1:
             return [palette[main_colour]]
@@ -35,16 +50,21 @@ def pick_colours_in_one_family(num_sub_colours: int, main_colour: str) -> list:
                 palette[f'{main_colour}-darker'],
             ]
         case _:
-            raise ValueError('The number of colours to pick must be between 1 and 5.')
+            raise ValueError('The number of colours to pick must be between '
+                             '1 and 5.')
 
 
-def pick_colours(num_colour_family: int, num_sub_colours: int, main_colour: str) -> list:
+def pick_colours(
+        num_colour_family: int,
+        num_sub_colours: int | list,
+        main_colour: str
+) -> list:
     """Pick colours from the palette.
 
     Args:
         num_colour_family (int): The number of colour families to pick.
-        num_sub_colours (int): The number of sub colours in each colour
-                               families to pick.
+        num_sub_colours (int | list): The number of sub colours in each colour
+                                      families to pick.
         main_colour (str): The main colour to pick. You may use the name
                            of colour in the main palette (e.g., silverstone)
                            or the colour (e.g., yellow).
@@ -55,7 +75,8 @@ def pick_colours(num_colour_family: int, num_sub_colours: int, main_colour: str)
 
     Example:
         pick_colours(2, 2, 'monza')
-        # Output: [['#f46255', '#b32414'], ['#ff9900', '#de6000']]
+
+        Output: [['#f46255', '#b32414'], ['#ff9900', '#de6000']]
     """
     if main_colour in name:
         main_colour = name[main_colour]
@@ -63,9 +84,11 @@ def pick_colours(num_colour_family: int, num_sub_colours: int, main_colour: str)
         raise ValueError('The main colour is not in the palette.')
 
     if num_colour_family < 1:
-        raise ValueError('The number of colour families to pick must be at least 1.')
+        raise ValueError('The number of colour families to pick must be at '
+                         'least 1.')
     elif num_colour_family > len(main_palette):
-        raise ValueError('The number of colour families to pick must be at most the number of main colours.')
+        raise ValueError('The number of colour families to pick must be at '
+                         'most the number of main colours.')
 
     # Handle the colour families
     colour_families = [main_colour]
@@ -76,6 +99,13 @@ def pick_colours(num_colour_family: int, num_sub_colours: int, main_colour: str)
             continue
         colour_families.append(i)
 
-    return [pick_colours_in_one_family(num_sub_colours, colour) for colour in colour_families]
-
+    if isinstance(num_sub_colours, int):
+        return [pick_colours_in_one_family(num_sub_colours, colour)
+                for colour in colour_families]
+    elif isinstance(num_sub_colours, list):
+        return [pick_colours_in_one_family(num_sub_colours[i], colour)
+                for i, colour in enumerate(colour_families)]
+    else:
+        raise ValueError('The number of sub colours must be an integer or a '
+                         'list of integers.')
 
