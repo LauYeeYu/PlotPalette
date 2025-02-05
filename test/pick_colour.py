310cc813c6de0b20e1404ddccef08855970e9c22
Lau Yee-Yu <liuyiyu2003@126.com>
2025-02-06 00:29:42 +0800

    test: change the code to make it better show the functionality

--- test/pick_colour.py
+++ test/pick_colour.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     # Create a list of all the main colours
-    picked_colours = plotpalette.pick_colours(2, 2, 'monza')
+    picked_colours = plotpalette.pick_colours(2, 3, 'monza')
     colours = [colour for colour_family in picked_colours for colour in colour_family]
     names = [str(i) for i in range(len(colours))]
     print(colours)
@@ -17,7 +17,7 @@ if __name__ == '__main__':
     fig, ax = plt.subplots(figsize=(15,5))
     ax.bar(x_pos, height, color=colours, hatch='', edgecolor='white')
     ax.set_xticks(x_pos)
-    ax.set_xticklabels(names, rotation=45, ha='right')
+    ax.set_xticklabels(names)
     ax.set_yticks([])
 
     # Show the plot
